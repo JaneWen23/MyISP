8e49fdaf2cecb0b23adede6e3afcb9f86158adf2
Jane Wen <jinghuanwen@outlook.com>
2023-11-18 12:40:03 +0800

    link opencv lib to the project
    Signed-off-by: Jane Wen <jinghuanwen@outlook.com>

---
+++ src/Infra/ImageIO.cpp
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// #include <opencv2/opencv.hpp>
+// using namespace cv;
+// void test_opencv()
+// {
+//     Mat image;
+//     image = imread( "anya18.png", IMREAD_COLOR );
+//     if ( !image.data )
+//     {
+//         printf("No image data \n");
+//         return -1;
+//     }
+//     namedWindow("Display Image", WINDOW_AUTOSIZE );
+//     imshow("Display Image", image);
+//     waitKey(0);
+// }
--- top.cpp
+++ top.cpp
@@ -5,11 +5,25 @@
 #include "src/DWT/dwt.hpp"
 #include "src/Filter/SlidingWindow.hpp"
 
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
 using namespace std;
 
 int main(){
-    test_dwt();
-    //test_config_kernel();
+    Mat image;
+    image = imread( "anya18.png", IMREAD_COLOR );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+     waitKey(0);
+
+
+    //test_dwt();
     std::cout<<"hello world!!\n";
     std::cout<<"hello Cpp!!\n";
     return 0;
